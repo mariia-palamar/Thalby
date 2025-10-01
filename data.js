1f9aa5a4fc7a76253d1b4596407e1d4ab4ed1919
Mariia Palamar <mariiapalamar05@gmail.com>
2025-10-01 14:04:01 +0300

    chore(data): book prices have been changed

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 export const guideBooks = [
     { id: 1, image: "./img/French-Culture-and-Heritage.jpg", title: "Thalby Guide to French Culture and Heritage", alt: "French Culture and Heritage", price: "6.99"},
-    { id: 2, image: "./img/French-People-and-Society.jpg", title: "Thalby Guide to French People and Society", alt: "French People and Society", price: "6.99"},
-    { id: 3, image: "./img/German-Culture-and-Heritage.jpg", title: "Thalby Guide to German Culture and Heritage", alt: "German Culture and Heritage", price: "6.99"}
+    { id: 2, image: "./img/French-People-and-Society.jpg", title: "Thalby Guide to French People and Society", alt: "French People and Society", price: "4.55"},
+    { id: 3, image: "./img/German-Culture-and-Heritage.jpg", title: "Thalby Guide to German Culture and Heritage", alt: "German Culture and Heritage", price: "8.49"}
 ]
